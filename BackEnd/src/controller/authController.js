8abf466139d3af7db3dad3698fe798aa80b62d0a
Lukitas <flucasyoel@outlook.es>
2024-02-08 14:20:30 -0300

    Add Authentication Controller

---
+++ BackEnd/src/controller/authController.js
@@ -0,0 +1,29 @@
+const { createUser } = require("../database/querydb");
+
+exports.login = (req, res) => {};
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      status: "FAILED",
+      data: { error: "Todos los datos son obligatorios" },
+    });
+  }
+
+  try {
+    const response = await createUser(username, email, password);
+    return res.status(200).json({
+      status: "OK",
+      data: response,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: "FAILED",
+      data: { error: err },
+    });
+  }
+};
+
+exports.logout = (req, res) => {};
