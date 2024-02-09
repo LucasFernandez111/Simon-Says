710310d9cc5dc3334b8a4808cfa2c7035bd4ea4c
Lukitas <flucasyoel@outlook.es>
2024-02-09 16:45:44 -0300

    Fix cors

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routers/router"));
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on PORT :3000");
 });
