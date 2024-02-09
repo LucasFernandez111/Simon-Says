58ab6e2014b176d130e42086a84951df94808083
Lukitas <flucasyoel@outlook.es>
2024-02-09 16:51:34 -0300

    fix

--- FrondEnd/src/components/ColorCard.jsx
+++ FrondEnd/src/components/ColorCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const ColorCard = ({ color, handleClick, colorCurrent, border }) => {
+const ColorCard = ({ color, handleClick, colorCurrent, border, bg }) => {
   return (
     <button
-      className={`w-48 h-48 bg-${color}-500 ${border} duration-200 m-1 hover:scale-95 hover:brightness-50  ${
+      className={`w-48 h-48 ${bg} ${border} duration-200 m-1 hover:scale-95 hover:brightness-50  ${
         colorCurrent === color && "brightness-200"
       }`}
       onClick={() => handleClick(color)}
--- FrondEnd/src/components/SimonSays.jsx
+++ FrondEnd/src/components/SimonSays.jsx
@@ -1,15 +1,31 @@
 import { useEffect, useState } from "react";
 import ColorCard from "./ColorCard";
+import { useNavigate } from "react-router-dom";
 
-function SimonDice() {
+import React from "react";
+import axios from "../api/axios";
+
+const SimonSays = () => {
   const colors = ["red", "green", "blue", "yellow"];
-
+  const navigate = useNavigate();
   const [sequence, setSequence] = useState([]);
   const [playerSequence, setPlayerSequence] = useState([]);
   const [colorCurrent, setColorCurrent] = useState(null);
   const [score, setScore] = useState(0);
   const [gameStart, setGameStart] = useState(false);
   const [gameOver, setGameOver] = useState(false);
+  const [scoreMax, setScoreMax] = useState(0);
+  const [userData, setUserData] = useState({ username: "", score: "" });
+
+  const logOut = async () => {
+    try {
+      const response = await axios.get("/logout");
+      console.log(response);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const addNewColor = () => {
     const numberRandom = Math.floor(Math.random() * 4);
@@ -46,45 +62,88 @@ function SimonDice() {
     let gameOver = false;
 
     playerSequence.forEach((item, i) => {
-      if (item != sequence[i]) {
-        gameOver = true;
-        setGameStart(false);
-        setScore(0);
-        setGameOver(true);
+      if (item == sequence[i]) {
         return;
       }
+      gameOver = true;
+      setGameStart(false);
+      setPlayerSequence([]);
+      setSequence([]);
+      setScore(0);
+      setGameOver(true);
+      return;
     });
 
-    if (!gameOver) {
-      if (playerSequence.length === sequence.length) {
-        setScore(score + 1);
-        setPlayerSequence([]);
+    if (!(!gameOver && playerSequence.length === sequence.length)) {
+      return;
+    }
+    setScore(score + 1);
+    setPlayerSequence([]);
 
-        addNewColor();
-      }
+    addNewColor();
+  };
+
+  const getData = async () => {
+    try {
+      const { data } = await axios.get("/datauser");
+      setUserData(data.data);
+      setScoreMax(data.data.score);
+    } catch (error) {}
+  };
+
+  const updateProgress = async () => {
+    try {
+      const response = await axios.put("/updateScore", { score });
+      console.log(response);
+      getData();
+    } catch (error) {
+      console.log(error);
     }
   };
 
+  useEffect(() => {
+    if (score > scoreMax) updateProgress();
+  }, [score]);
   useEffect(() => {
     if (playerSequence.length <= 0) return;
     checkSequence();
   }, [playerSequence, sequence]);
 
   useEffect(() => {
-    showSequence(); //Muestra la secuencia ;
+    if (gameStart) {
+      showSequence();
+    }
   }, [sequence]);
 
+  useEffect(() => {
+    getData();
+  });
+
   return (
-    <div className="flex justify-center items-center bg-neutral-900 w-screen h-screen ">
+    <div className="flex justify-center items-center flex-col gap-2 bg-neutral-900 w-screen h-screen ">
+      <h1 className="absolute left-6 top-6 font-semibold text-white  p-2 rounded-xl">
+        Usuario: {userData.username}
+      </h1>
+      <button
+        onClick={logOut}
+        className="absolute right-6 top-6 font-semibold text-white bg-gradient-to-r from-yellow-500 via-red-500 to-emerald-500 p-2 rounded-xl"
+      >
+        Log Out
+      </button>
+      <div>
+        <h1 className="font-semibold text-white ">SCORE MAX : {scoreMax}</h1>
+      </div>
       <div className="relative flex flex-col justify-center items-center">
         <div>
           <ColorCard
+            bg="bg-red-500"
             border="rounded-tl-full"
             color="red"
             colorCurrent={colorCurrent}
             handleClick={handleClick}
           />
           <ColorCard
+            bg="bg-green-500"
             border="rounded-tr-full"
             color="green"
             colorCurrent={colorCurrent}
@@ -93,12 +152,14 @@ function SimonDice() {
         </div>
         <div>
           <ColorCard
+            bg="bg-blue-500"
             border="rounded-bl-full"
             color="blue"
             colorCurrent={colorCurrent}
             handleClick={handleClick}
           />
           <ColorCard
+            bg="bg-yellow-500"
             border="rounded-br-full"
             color="yellow"
             colorCurrent={colorCurrent}
@@ -114,6 +175,6 @@ function SimonDice() {
       </button>
     </div>
   );
-}
+};
 
-export default SimonDice;
+export default SimonSays;
