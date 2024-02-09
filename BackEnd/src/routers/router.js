65fbf0ba355cd719c1f7f1c9c91aaeffe71c12b0
Lukitas <flucasyoel@outlook.es>
2024-02-09 16:45:29 -0300

    Add routes

--- BackEnd/src/routers/router.js
+++ BackEnd/src/routers/router.js
@@ -7,6 +7,10 @@ router.post("/register", authController.register);
 
 router.post("/login", authController.login);
 
-router.post("/logout", authController.logout);
+router.get("/datauser", authController.isAuth);
+
+router.put("/updateScore", authController.updateScore);
+
+router.get("/logout", authController.logout);
 
 module.exports = router;
