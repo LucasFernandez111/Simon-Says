d6c94f9175f4d1b5923e2fd967126a0eaf20a5ae
Lucas <flucasyoel@gmail.com>
2024-02-20 04:11:21 -0300

    Clean Code

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,13 +1,8 @@
-const express = require("express");
 require("dotenv").config({ path: "./env/.env" });
-
+const express = require("express");
 const cors = require("cors");
-console.log(process.env.DB_USER);
-
 const cookieParser = require("cookie-parser");
-
 const app = express();
-console.log(process.env.DB_USER);
 const corsOptions = {
   origin: true,
   credentials: true,
@@ -20,5 +15,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", require("./routers/router"));
 
 app.listen(3000, () => {
-  console.log("Server is running on PORT :3000");
+  console.info("Server is running on PORT :3000");
 });
